823cf62d16fcc2fb937b3302592ee28d19bb0770
d5c5ceb0 <d5c5ceb0@gmail.com>
2024-01-22 14:38:27 +0800

    image

--- components/DiscordLogo.jsx
+++ components/DiscordLogo.jsx
@@ -2,5 +2,5 @@ import React from "react";
 import { Image } from "@nextui-org/react";
 
 export const DiscordLogo = () => (
-  <Image width={36} height={30} src="/discord.png" alt="logo" />
+  <Image width={36} height={30} src="./discord.png" alt="logo" />
 );
